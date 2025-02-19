ed92fe7c281eee099d10588608a4d2e278887649
Fernando <correo>
2025-02-19 20:33:05 +0100

    Cargando mas ejs

---
+++ clases clase1/Variables.py
@@ -0,0 +1,26 @@
+nombre = "Juan"
+print("NOMBRE: "+ nombre)
+
+len(nombre)
+
+edad = 99
+
+print("EDAD: "+ str(edad))
+
+isConductor = True 
+isAnciano = False
+
+print("CONDUCTOR: "+str(isConductor))
+print("ANCIANO: "+str(isAnciano))
+
+datoNulo = None
+
+
+numero1 = 4
+numero2 = 6
+
+print(numero1+numero2)
+print(numero1-numero2)
+print(numero2/numero1)
+print(numero2//numero1)
+print(numero1**0.5)
---
+++ clases clase1/textos.py
@@ -0,0 +1,10 @@
+print("Texto 1")
+print()
+print('Texto 2')
+
+#comentario de una línea
+
+'''
+comnetario1
+comentario2
+'''
